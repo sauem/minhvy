2787e889e6389c6d3c09487ba77fc71fd2eaf948
sauem <nguyendinhthang.go97@gmail.com>
2023-10-05 11:59:48 +0700

    update

--- wp-content/themes/minhvytheme/index.php
+++ wp-content/themes/minhvytheme/index.php
@@ -1,12 +1,2 @@
 <?php
-get_header();
-
-?>
-
-    <div class="section section-padding">
-        <div class="container">
-            <?php the_content(); ?>
-        </div>
-    </div>
-<?php
-get_footer();
+include_once 'front-page.php';
--- wp-content/themes/minhvytheme/page.php
+++ wp-content/themes/minhvytheme/page.php
@@ -10,16 +10,11 @@
  */
 
 get_header();
-
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
-
+?>
+    <div class="section section-padding">
+        <div class="container">
+            <?php the_content(); ?>
+        </div>
+    </div>
+<?php
 get_footer();
